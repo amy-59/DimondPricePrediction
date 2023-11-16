abeb5c2da4adafb23893e5db5fbda7bd59149f9c
Amy <ap.amruta59@gmail.com>
2023-11-16 12:09:39 +0530

    exception file added

--- src/DimondPricePrediction/logger.py
+++ src/DimondPricePrediction/logger.py
@@ -13,3 +13,6 @@ LOG_FILEPATH = os.path.join(log_path,LOG_FILE)
 logging.basicConfig(level=logging.INFO,
                     filename=LOG_FILEPATH,
                     format='[%(asctime)s %(lineno)d %(name)s - %(levelname)s - %(message)s]')
+
+# if __name__ == '__main__':
+#     logging.info("First test")
